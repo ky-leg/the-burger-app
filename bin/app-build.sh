3651a5e40acf7d1ca938901aed00178a25e85137
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-12-04 20:13:00 -0500

    modifies bin task

---
+++ bin/app-build.sh
@@ -0,0 +1,12 @@
+# exit on error
+set -o errexit
+
+bundle install
+# clean
+rm -rf public
+# build
+npm install --prefix client && npm run build --prefix client
+# migrate
+bundle exec rake db:migrate
+# postbuild
+cp -a client/build/. public/
